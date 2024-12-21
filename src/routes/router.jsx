fc631e4a367f76c00cb158af01679eb6c3f31cd2
MethuParoi <paroimethu@gmail.com>
2024-12-21 23:43:30 +0600

    private route setup successfully

---
+++ src/routes/router.jsx
@@ -0,0 +1,73 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+import PrivateRoute from "./PrivateRoute";
+import Home from "../pages/Home";
+import AvailableFoods from "../pages/AvailableFoods";
+import AddFood from "../pages/AddFood";
+import ManageFood from "../pages/ManageFood";
+import FoodRequest from "../pages/FoodRequest";
+// import Home from "../pages/Home";
+// import UpdateProfile from "../components/profile/UpdateProfile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/available-foods",
+        element: <AvailableFoods />,
+      },
+      {
+        path: "/add-food",
+        element: (
+          <PrivateRoute>
+            <AddFood />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/manage-food",
+        element: (
+          <PrivateRoute>
+            <ManageFood />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/food-requests",
+        element: (
+          <PrivateRoute>
+            <FoodRequest />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Register />,
+  },
+  //   {
+  //     path: "/forgot-password",
+  //     element: <ForgotPassword />,
+  //   },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
